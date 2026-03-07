50d2bce430415bd3e699bb2fbce0d0b375907387
ArianaPita-0102 <ariana.pita@ucb.edu.bo>
2026-03-07 17:05:14 -0400

    Refactor tests to use describe/it structure instead of individual tests

--- src/totalizador.test.js
+++ src/totalizador.test.js
@@ -1,13 +1,15 @@
 import totalizador from "./totalizador.js";
 
-test("muestra la cantidad ingresada", () => {
-  expect(totalizador.mostrarCantidad(5)).toEqual("Cantidad ingresada: 5");
-});
+describe("Totalizador", () => {
+  it("Deberia mostrar la cantidad ingresada", () => {
+    expect(totalizador.mostrarCantidad(5)).toEqual("Cantidad ingresada: 5");
+  });
 
-test("muestra el precio ingresado", () => {
-  expect(totalizador.mostrarPrecioItem(10)).toEqual("Precio por item ingresado: 10");
-});
+  it("Deberia mostrar el precio ingresado", () => {
+    expect(totalizador.mostrarPrecioItem(10)).toEqual("Precio por item ingresado: 10");
+  });
 
-test("muestra el precio neto", () => {
-  expect(totalizador.mostrarPrecioNeto(5, 10)).toEqual(50);
+  it("Deberia mostrar el precio netro", () => {
+    expect(totalizador.mostrarPrecioNeto(5, 10)).toEqual(50);
+  });
 });
